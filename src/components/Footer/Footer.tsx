fcac489dd0b8478ffb576f09c702e1322eec13bb
Nikki NoLife <nikkinolife@gmail.com>
2023-06-11 20:40:24 +0400

    feat: footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -10,10 +10,28 @@ export default function Footer() {
         <div className={styles.footer__container}>
           <Logo />
         </div>
-        <div className={styles.footer__container}>Пользователю</div>
-        <div className={styles.footer__container}>Популярные категории</div>
-        <div className={styles.footer__container}>Команда</div>
         <div className={styles.footer__container}>
+          <h3>Пользователю</h3>
+          <a href="/">Связаться с нами</a>
+          <a href="/">Поддержка пользователей</a>
+          <a href="/">FAQ & Руководства</a>
+          <a href="/">Политика конфиденциальности</a>
+          <a href="/">Пользовательское соглашение</a>
+        </div>
+        <div className={styles.footer__container}>
+          <h3>Популярные категории</h3>
+          <a href="/">Смартфоны</a>
+          <a href="/">Teteras electricas</a>
+          <a href="/">Стиральные машины</a>
+          <a href="/">Телевизоры</a>
+          <a href="/">Ноутбуки</a>
+        </div>
+        <div className={styles.footer__container}>
+          <h3>Команда</h3>
+          <a href="/">О нас</a>
+          <a href="/">Работа у нас</a>
+        </div>
+        <div className={styles.footer__copyright}>
           Copyright © 2023 Behoof, Inc. Все права защищены
         </div>
       </div>
